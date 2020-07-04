8e276bba6a157a37e748c6c925f46e9624847908
miguelrueda <miguel.rueda.e@thalesdigital.io>
2020-07-04 12:42:38 -0500

    Added User Defaults

--- iOsDataPersistence/ViewController.swift
+++ iOsDataPersistence/ViewController.swift
@@ -10,11 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var departmentTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        nameTextField.text
+            = UserDefaults.standard.string(forKey: "name")
+        emailTextField.text
+        = UserDefaults.standard.string(forKey: "email")
+        departmentTextField.text
+        = UserDefaults.standard.string(forKey: "department")
     }
 
-
+    @IBAction func saveButtonTapped(_ sender: Any) {
+        UserDefaults.standard.set(nameTextField.text, forKey: "name")
+        UserDefaults.standard.set(emailTextField.text, forKey: "email")
+        UserDefaults.standard.set(departmentTextField.text, forKey: "department")
+    }
 }
 
